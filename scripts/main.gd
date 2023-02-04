1fd3cdddc8fcee464b802aed164d10ed7439c4ec
yoshino.ittetsu <yoshino.ittetsu.0427@gmail.com>
2023-02-04 20:23:15 +0900

    screen transition implementation

---
+++ scripts/main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# func _on_Button_pressed():
+#   TODO: 何かの条件で画面遷移する
+#    get_tree().change_scene("res://scenes/ending.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
