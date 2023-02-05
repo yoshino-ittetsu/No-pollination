9079a9fbe7e94d71afd896586c6dfe0a26486a75
yoshino.ittetsu <yoshino.ittetsu.0427@gmail.com>
2023-02-05 15:36:51 +0900

    wip: impliment debuff-tower

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-export(PackedScene) var tower_scene
+var ballet_tower_scen = load("res://scenes/BalletTower.tscn")
+var debuff_tower_scen = load("res://scenes/DebuffTower.tscn")
 var screen_size: Vector2
 
 func _ready():
-	screen_size = get_viewport_rect().size
-
+    screen_size = get_viewport_rect().size
 
 func _input(event):
-	if event is InputEventMouseButton and Input.is_action_pressed("drop_tower"):
-		var tower = tower_scene.instance()
-		tower.position = event.position
-		add_child(tower)
+    if event is InputEventMouseButton and Input.is_action_pressed("drop_ballet_tower"):
+        var ballet_tower = ballet_tower_scen.instance()
+        ballet_tower.position = event.position
+        add_child(ballet_tower)
+
+    if event is InputEventMouseButton and Input.is_action_pressed("drop_debuf_tower"):
+        var debuff_tower = debuff_tower_scen.instance()
+        debuff_tower.position = event.position
+        add_child(debuff_tower)
